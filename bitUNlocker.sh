d98d63556f1f91a2b6dea1edfb1b8259bbc0c71b
kxisxr <47679627+k4is3r13@users.noreply.github.com>
2021-07-03 21:01:58 -0500

    Update to use in Mac OS systems

--- bitUNlocker.sh
+++ bitUNlocker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 #Creator: kxisxr
 greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
@@ -9,15 +9,14 @@ purpleColour="\e[0;35m\033[1m"
 turquoiseColour="\e[0;36m\033[1m"
 grayColour="\e[0;37m\033[1m"
 
-
 echo -e "${greenColour}""
- _      _  _               __  _               _                
-| |__  (_)| |_  /\ /\   /\ \ \| |  ___    ___ | | __  ___  _ __ 
+ _      _  _               __  _               _
+| |__  (_)| |_  /\ /\   /\ \ \| |  ___    ___ | | __  ___  _ __
 | '_ \ | || __|/ / \ \ /  \/ /| | / _ \  / __|| |/ / / _ \| '__|
-| |_) || || |_ \ \_/ // /\  / | || (_) || (__ |   < |  __/| |   
-|_.__/ |_| \__| \___/ \_\ \/  |_| \___/  \___||_|\_\ \___||_|   
+| |_) || || |_ \ \_/ // /\  / | || (_) || (__ |   < |  __/| |
+|_.__/ |_| \__| \___/ \_\ \/  |_| \___/  \___||_|\_\ \___||_|
 
-by kxisxr                                                                
+by kxisxr
 @pixelbit131
 ""${endColour}"
 
@@ -29,6 +28,19 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+so=linux
+elif [[ "$OSTYPE" == "darwin"* ]]
+then
+so=mac
+else
+echo -e -n "${greenColour}"' Operating system not supported, exitting... '"${endColour}"
+exit 0
+fi
+
+if [ $so = "linux" ]
+then
 echo -e -n "${greenColour}"'Checking for dislocker...'"${endColour}"
 echo -e ' '
 sleep 0.5
@@ -37,7 +49,7 @@ if ! command -v dislocker &> /dev/null
 then
     echo -e -n "${greenColour}"'Installing' "${blueColour}"'dislocker...'"${endColour}""${endColour}"
     echo -e ' '
-    sudo apt-get install dislocker -y > /dev/null 2>&1
+    sudo apt-get install dislocker -y 2>/dev/null
     echo -e ' '
     sleep 0.5
 else
@@ -57,7 +69,6 @@ else
 echo "Directory created: in /mnt/bitlocker"
 mkdir bitlocker
 fi
-cd - | 0>&1
 echo -e ' '
 
 sleep 0.5
@@ -77,8 +88,6 @@ echo -e ' '
 echo -e -n "${yellowColour}"'Select the disk to unlock:' "${endColour}"
 read disk
 
-
-
 unset password
 prompt=$(echo -e -n "${yellowColour}"'Enter the password: '"${endColour}")
 echo -e ' '
@@ -96,6 +105,7 @@ sleep 0.5
 
 echo -e -n "${yellowColour}"'1.- Yes.\n'"${endColour}"
 echo -e -n "${yellowColour}"'2.- No.\n'"${endColour}"
+echo -e -n "${redColour}"'3.- Exit.\n'"${endColour}"
 echo -e ' '
 echo -e -n "${yellowColour}"'Create a new directory to mount?' "${endColour}"
 read var
@@ -105,19 +115,14 @@ then
 echo -e ' '
 echo -e -n "${yellowColour}"'Name: '"${endColour}"
 read newDir
-
-cd /mnt
-if [ -d $newDir ]
-then
-    echo -e "${redColour}"'This directory already exists!!!, skipping...'"${endColour}"
-else
 mkdir /mnt/$newDir
 echo -e ' '
 echo -e -n "${yellowColour}"'Directory created in /mnt/'$newDir"${endColour}"
 echo -e ' '
+else
+echo -e -n "${redColour}"'Skipping...'"${endColour}"
+echo -e ' '
 fi
-fi
-cd - | 0>&1
 
 echo -e ' '
 echo -e -n "${greenColour}"'Directories in /mnt :' "${endColour}"
@@ -129,17 +134,86 @@ read dir
 echo -e ' '
 sleep 0.5
 
+sudo dislocker -v -V /dev/$disk -u$password /mnt/bitlocker
+sleep 0.5
 
-if [ "$(ls -A /mnt/$dir)" ] 
+sudo mount --rw -o loop /mnt/bitlocker/dislocker-file /mnt/$dir
+sleep 0.5
+
+echo -e -n "${turquoiseColour}"'Mounted on: /mnt/'$dir"${endColour}"
+#----------------------------------------------------------------------------------------------------------------------------------------
+#Running on mac
+elif [ $so = "mac" ]
 then
-    echo -e "${redColour}"'Dir'"${endColour}" $dir "${redColour}"'is not Empty!, probably mounted already'"${endColour}"
-    echo -e "${greenColour}"'Exiting...'"${endColour}"
-    exit 0
-else
-	sudo dislocker -v -V /dev/$disk -u$password /mnt/bitlocker
-	sleep 0.5
-	sudo mount --rw -o loop /mnt/bitlocker/dislocker-file /mnt/$dir
-	sleep 0.5
+echo -e -n "${greenColour}"'Checking for dislocker...'"${endColour}"
+echo -e ' '
+sleep 0.5
 
-	echo -e -n "${turquoiseColour}"'Mounted on: /mnt/'$dir"${endColour}"
+if ! command -v dislocker &> /dev/null
+then
+    echo -e -n "${greenColour}"'Installing' "${blueColour}"'dislocker...'"${endColour}""${endColour}"
+    echo -e ' '
+    git clone https://github.com/Aorimn/dislocker 2>/dev/null
+    cd dislocker
+    brew update
+    brew install caskroom/cask/macfuse > /dev/null
+    brew install src/dislocker.rb > /dev/null
+    echo -e ' '
+    sleep 0.5
+else
+echo -e "${redColour}"'Dislocker exists, skipping...'"${endColour}"
+fi
+
+echo -e ' '
+sleep 0.5
+
+echo -e "${greenColour}"'Disks available: '"${endColour}"
+diskutil list external
+echo -e ' '
+
+show_disk=$(diskutil list | grep external | awk '{print $1}')
+show_disk2=$(diskutil list external | grep "[0-9]s[0-9]$" | awk '{print $6}')
+
+echo -e ' '
+echo -e "${greenColour}"'[*] Eligible encrypted disks:'"${endColour}" "${purpleColour}"$show_disk $show_disk2"${endColour}\n"
+echo -e ' '
+
+echo -e -n "${greenColour}"'Select the disk: '"${endColour}"
+read disk
+echo -e ' '
+
+echo -e -n "${greenColour}"'Name of the directory to mount: '"${endColour}"
+read dir
+echo -e ' '
+
+echo -e -n "${greenColour}"'Type the password: '"${endColour}"
+read pass
+echo -e ' '
+sleep 0.5
+
+echo -e -n "${greenColour}"'Creating the dislocker file...'"${endColour}"
+echo -e ' '
+sleep 0.5
+
+sudo dislocker -v -V $disk -u$pass -- $dir 2>/dev/null
+
+echo -e -n "${greenColour}"'Attaching the image... '"${endColour}"
+echo -e ' '
+sleep 0.5
+ 
+sudo hdiutil attach -imagekey diskimage-class=CRawDiskImage -nomount $dir/dislocker-file 2>/dev/null
+
+disk2=$(diskutil list | grep image | awk '{print $1}')
+format=$(diskutil info $disk2 | grep "Bundle" | awk '{print $3}')
+sleep 1
+
+echo -e "${greenColour}"'Mounting the disk...'"${endColour}"
+echo -e ' '
+
+sleep 0.5
+
+sudo mount -t $format $disk2 $dir 2>/dev/null
+
+echo -e -n "${purpleColour}"'Disk mounted on:'"${endColour}" "${turquoiseColour}"$PWD/$dir "${endColour}"
+
 fi
