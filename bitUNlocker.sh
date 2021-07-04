f1a51a61bb5264e5171e816af77ab974ff56981f
kxisxr <47679627+k4is3r13@users.noreply.github.com>
2021-07-03 21:33:50 -0500

    Update bitUNlocker.sh

--- bitUNlocker.sh
+++ bitUNlocker.sh
@@ -1,13 +1,13 @@
-#!/bin/zsh
+#!/bin/bash
 #Creator: kxisxr
-greenColour="\e[0;32m\033[1m"
-endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+greenColour="\x1B[0;32m\033[1m"
+endColour="\033[0m\x1B[0m"
+redColour="\x1B[0;31m\033[1m"
+blueColour="\x1B[0;34m\033[1m"
+yellowColour="\x1B[0;33m\033[1m"
+purpleColour="\x1B[0;35m\033[1m"
+turquoiseColour="\x1B[0;36m\033[1m"
+grayColour="\x1B[0;37m\033[1m"
 
 echo -e "${greenColour}""
  _      _  _               __  _               _
